d9090b7954c2fa808b895123b14dc84dacf1d4c5
emmanueldav <kuradavis987@gmail.com>
2023-07-27 15:40:01 +0100

    added link and 404 page

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8dbv7s%lyp@7#pdj-j^tgpgt#ag(jmhgv%xwqgvtpn=ipw=!k0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
