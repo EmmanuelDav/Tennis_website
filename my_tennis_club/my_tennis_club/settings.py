fc55cfc50043b9ca25a1b8663a9729d878deb624
emmanueldav <kuradavis987@gmail.com>
2023-11-04 21:37:39 +0100

    migrated to aws

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -35,6 +39,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'mystaticfolder'
 ]
+
 
 # Application definition
 
@@ -85,10 +90,15 @@ WSGI_APPLICATION = 'my_tennis_club.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':  'postgres',
+        'USER':  os.environ.get("USER"),
+        'PASSWORD':  os.environ.get("PASSWORD"),
+        'HOST':  os.environ.get("HOST"),
+        'PORT':  os.environ.get("PORT"),
     }
 }
+
 
 
 # Password validation
